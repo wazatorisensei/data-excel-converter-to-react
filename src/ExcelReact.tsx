f08fcd4b10d7ca90dbd1f94bf7addd9c7ecb1310
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2023-02-17 02:30:31 -0300

    work_on: ExcelReact to testing

--- src/ExcelReact.tsx
+++ src/ExcelReact.tsx
@@ -1,8 +1,35 @@
-import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+
+import {
+  handleFileSelection,
+  getDataFromCSVFile,
+  handleDownloadCSV,
+  handleFileSelect,
+  TableComponent,
+  ITableData,
+} from "./shared";
 
 export const ExcelReact = () => {
 
+  const data = [
+    {name: 'teste 1', age: 'teste 1'},
+  ];
+
+  const [tableData, setTableData] = useState<ITableData[]>([]);
+
+  // const [tableData, setTableData] = useState<string | null>(null);
+
+  useEffect(() => {
+    // getDataFromCSVFile();
+    // handleDownloadCSV();
+    // handleFileSelection();
+    // handleFileSelect();
+    // setTableData(data);
+  }, []);
+
   return (
-    <Box>Teste</Box>
+    <TableComponent
+      data={tableData}
+    />
   );
 };
