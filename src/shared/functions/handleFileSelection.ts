1bbbc5a80d9e97c784c4e4f7ce36c2554bd3f9e0
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2023-02-17 02:07:42 -0300

    feat: function handle file selection

---
+++ src/shared/functions/handleFileSelection.ts
@@ -0,0 +1,21 @@
+export {};
+
+// import { ChangeEvent, useState } from "react";
+// import { ITableData } from "../interfaces";
+
+// const [tableData, setTableData] = useState<ITableData[]>([]);
+
+// export function handleFileSelection(event: ChangeEvent<HTMLInputElement>) {
+//   const file = event.target.files && event.target.files[0];
+//   if (file) {
+//     const reader = new FileReader();
+//     reader.onload = async (event) => {
+//       const result = event.target?.result;
+//       if (typeof result === "string") {
+//         const data = getDataFromCSVFile(result as File);
+//         setTableData(await data);
+//       }
+//     };
+//     reader.readAsText(file);
+//   }
+// }
