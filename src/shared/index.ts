6c6e86418e69a2bf932ad2a8966b22bbebae9871
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2023-02-17 02:29:51 -0300

    add: all imports shared

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,1 +1,12 @@
-export { };
+// components
+export * from './components/index';
+
+// functions
+export * from './functions/handleFileSelection';
+export * from './functions/getDataFromCSVFile';
+export * from './functions/handleDownloadCSV';
+export * from './functions/handleFileSelect';
+export * from './functions/tableToCSV';
+
+// interfaces
+export * from './interfaces/index';
